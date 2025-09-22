ce2b1385efed8caf78ecbfbec11e176b7724fbe5
rithin-rajpoot <rithinrajpoot@gmail.com>
2025-09-22 18:37:59 +0530

    Updated Loading component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -40,13 +40,13 @@ const LoadingSpinner = () => (
         </div>
         
         {/* Loading dots animation */}
-        <div className="flex justify-center items-center mt-4">
+        {/* <div className="flex justify-center items-center mt-4">
           <div className="flex space-x-1">
             <div className="w-2 h-2 bg-slate-400 rounded-full animate-bounce" style={{animationDelay: '0ms'}}></div>
             <div className="w-2 h-2 bg-slate-400 rounded-full animate-bounce" style={{animationDelay: '150ms'}}></div>
             <div className="w-2 h-2 bg-slate-400 rounded-full animate-bounce" style={{animationDelay: '300ms'}}></div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   </div>
